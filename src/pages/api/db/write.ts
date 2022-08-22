70ef53dcca99def7cca449c1d730b102388847da
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-08-22 15:43:36 +0100

    remove aws prefix

--- src/pages/api/db/write.ts
+++ src/pages/api/db/write.ts
@@ -3,10 +3,10 @@ import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb'
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 
 const dbClient = new DynamoDBClient({  
-    region: process.env.AWS_REGION,
+    region: process.env.REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        accessKeyId: process.env.ACCESS_KEY_ID!,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY!,
     },
 });
 
